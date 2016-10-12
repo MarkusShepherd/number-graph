74fc9cb5e9e4c90a6a52cd8db493d733a2dc8927
Markus Shepherd <markus.r.shepherd@gmail.com>
2016-10-12 17:19:21 +0300

    added --start option; added support for roman

--- load.py
+++ load.py
@@ -9,19 +9,19 @@ from literumi import spell
 
 DRIVER = GraphDatabase.driver("bolt://localhost", auth=basic_auth("neo4j", "numbers"))
 
-def create_links(lang, words=None, max_number=None):
+def create_links(lang, words=None, start=0, end=None):
     if not lang:
         raise ValueError('no language provided')
 
     session = DRIVER.session()
 
     if not words:
-        if not max_number:
+        if not end:
             raise ValueError('max number is required if no word list is given')
 
-        words = (spell(n, lang=lang) for n in range(max_number + 1))
+        words = (spell(n, lang=lang) for n in range(start, end + 1))
 
-    for n1, word in enumerate(words):
+    for n1, word in enumerate(words, start=start):
         n2 = len(word.replace(' ', '').replace('-', '').replace(',', ''))
         cypher = '''
             MERGE (n1:Number {{value: {n1}}})
@@ -39,21 +39,23 @@ def create_links(lang, words=None, max_number=None):
         for record in result:
             print(record.values())
 
-        if max_number and n1 >= max_number:
+        if end and n1 >= end:
             break
 
     session.close()
 
 def get_args():
-    parser = argparse.ArgumentParser(description='description')
+    parser = argparse.ArgumentParser(description='load number word length graph data into Neo4j')
     parser.add_argument('-l', '--lang', required=True,
-        help='target language')
+                        help='target language')
     parser.add_argument('-f', '--file',
-        help='file containing the words')
-    parser.add_argument('-n', '--numbers', type=int, default=1000,
-        help='numbers to be processed')
+                        help='file containing the words')
+    parser.add_argument('-s', '--start', type=int, default=0,
+                        help='first number to be processed')
+    parser.add_argument('-e', '--end', type=int, default=1000,
+                        help='last number to be processed')
     parser.add_argument('-v', '--verbose', action='count', default=0,
-        help='increase verbosity (specify multiple times for more)')
+                        help='increase verbosity (specify multiple times for more)')
 
     return parser.parse_args()
 
@@ -66,15 +68,7 @@ def main():
     else:
         words = None
 
-    create_links(lang=args.lang, words=words, max_number=args.numbers)
+    create_links(lang=args.lang, words=words, start=args.start, end=args.end)
 
 if __name__ == '__main__':
     main()
-
-'''
-MATCH (n1:Number)
-WHERE (n1)<-[:LINK {lang: 'eo'}]-(:Number)
-OPTIONAL MATCH (n1)<-[e1:LINK {lang: 'eo'}]-(n2:Number)
-WHERE (n2)<-[:LINK {lang: 'eo'}]-(:Number)
-RETURN n1, e1, n2
-'''
